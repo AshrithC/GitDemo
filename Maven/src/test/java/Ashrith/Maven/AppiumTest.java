4086f6729993f29db50caa02e9b80c68b10d9bcf
Ashrith <ashrithc.ac@gmail.com>
2020-09-14 15:40:39 +0530

    first commit

---
+++ Maven/src/test/java/Ashrith/Maven/AppiumTest.java
@@ -0,0 +1,17 @@
+package Ashrith.Maven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void openApp() {
+		System.out.println("OpenApp");
+	}
+	
+	@Test
+	public void closeApp() {
+		System.out.println("closeApp");
+	}
+
+}
---
+++ Maven/src/test/java/Ashrith/Maven/SeleniumTest.java
@@ -0,0 +1,16 @@
+package Ashrith.Maven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void browser() {
+		System.out.println("browser");
+	}
+	
+	@Test
+	public void WindowUI() {
+		System.out.println("windowUI");
+	}
+
+}
